e0e48401eecb42a1c4e810b63a032bb2f00cdf6f
tkmttkm <tkmttkm322@icloud.com>
2024-02-26 07:46:16 +0900

    delete Setter

--- src/main/java/com/example/demo/Entity/DataJDBCEntity.java
+++ src/main/java/com/example/demo/Entity/DataJDBCEntity.java
@@ -6,7 +6,6 @@ import org.springframework.data.relational.core.mapping.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * DataJDBCテスト用
@@ -14,17 +13,13 @@ import lombok.Setter;
  *
  */
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table("TEST")
 public class DataJDBCEntity {
 	@Id
 	private Integer id;
-
 	private String firstName;
-
 	private String lastName;
-
 	private Integer birthDay;
 }
