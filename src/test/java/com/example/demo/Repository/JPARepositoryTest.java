443b3eac182eaf476618d3ebd03306fb9068e8c0
tkmttkm <tkmttkm322@icloud.com>
2024-02-28 07:55:16 +0900

    add test

--- src/test/java/com/example/demo/Repository/JPARepositoryTest.java
+++ src/test/java/com/example/demo/Repository/JPARepositoryTest.java
@@ -29,7 +29,10 @@ class JPARepositoryTest {
 		List<JPAEntity> data = repository.findAll();
 		
 		assertEquals(data.size(), 4);
+		assertEquals(data.get(0).getId(), 1);
+		assertEquals(data.get(0).getFirst_name().strip(), "テスト");
 		assertEquals(data.get(0).getLast_name().strip(), "太郎");
+		assertEquals(data.get(0).getBirth_day(), 20240101);
 	}
 
 	@Test
@@ -37,8 +40,10 @@ class JPARepositoryTest {
 		Optional<JPAEntity> dataOpt = repository.findById(1);
 		JPAEntity data = dataOpt.orElse(null);
 		
-		assertEquals(data.getBirth_day(), 20240101);
+		assertEquals(data.getId(), 1);
+		assertEquals(data.getFirst_name().strip(), "テスト");
 		assertEquals(data.getLast_name().strip(), "太郎");
+		assertEquals(data.getBirth_day(), 20240101);
 	}
 	
 	@Test
@@ -47,6 +52,7 @@ class JPARepositoryTest {
 		
 		JPAEntity insertEntity = new JPAEntity(9,"テストくん", "テストちゃん", 20200202);
 		repository.save(insertEntity);
+		
 		assertTrue(repository.existsById(9));
 		
 		Optional<JPAEntity> insertDataOpt = repository.findById(9);
